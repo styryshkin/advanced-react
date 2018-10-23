390f89695ec0924fb70ad72bf6223594b0d48de0
Sergey Tyryshkin <styryshkin@gmail.com>
2018-10-23 22:01:25 +0300

    added frontend

--- frontend/components/Meta.js
+++ frontend/components/Meta.js
@@ -4,7 +4,7 @@ import Head from 'next/head';
 const Meta = () => (
   <Head>
     <meta name="viewport" content="width=device-width,initial-scale=1" />
-    <mate charset="utf-8" />
+    <mate charSet="utf-8" />
     <title>Online Store</title>
   </Head>
 );
--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,14 +1,61 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React from 'react';
+import styled, { ThemeProvider, createGlobalStyle } from 'styled-components';
 import Header from './Header';
 import Meta from './Meta';
 
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  white: '#FFFFFF',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0,0,0,0.9)',
+};
+
+const StyledPage = styled.div`
+  background: ${props => props.theme.white};
+  color: ${props => props.theme.black};
+`;
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: border-box;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: -apple-system, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+  a {
+    text-decoration: none;
+    color: ${props => props.theme.black}
+  }
+`;
+
 const Page = ({ children }) => (
-  <div>
-    <Header />
-    <Meta />
-    {children}
-  </div>
+  <ThemeProvider theme={theme}>
+    <StyledPage>
+      <Header />
+      <Meta />
+      <GlobalStyle />
+      <Inner>{children}</Inner>
+    </StyledPage>
+  </ThemeProvider>
 );
 
 Page.propTypes = {
